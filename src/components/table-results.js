c2c867189e996d2afd2ce19d253824fc357198d3
Johan Bonneau <johan.bonneau@mymoneybank.com>
2019-12-10 13:37:23 +0100

    Ajout d'une pagination.

--- src/components/table-results.js
+++ src/components/table-results.js
@@ -4,52 +4,163 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
 import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 const TableResults = ({results}) => {
+  const useStyles1 = makeStyles(theme => ({
+    root: {
+      flexShrink: 0,
+      marginLeft: theme.spacing(2.5),
+    },
+  }));
+
+  function TablePaginationActions(props) {
+    const classes = useStyles1();
+    const theme = useTheme();
+    const { count, page, rowsPerPage, onChangePage } = props;
+  
+    const handleFirstPageButtonClick = event => {
+      onChangePage(event, 0);
+    };
+  
+    const handleBackButtonClick = event => {
+      onChangePage(event, page - 1);
+    };
+  
+    const handleNextButtonClick = event => {
+      onChangePage(event, page + 1);
+    };
+  
+    const handleLastPageButtonClick = event => {
+      onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+    };
+  
+    return (
+      <div className={classes.root}>
+        <IconButton
+          onClick={handleFirstPageButtonClick}
+          disabled={page === 0}
+          aria-label="première page"
+        >
+          {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+        </IconButton>
+        <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="page précédente">
+          {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+        </IconButton>
+        <IconButton
+          onClick={handleNextButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="page suivante"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+        </IconButton>
+        <IconButton
+          onClick={handleLastPageButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="dernière page"
+        >
+          {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+        </IconButton>
+      </div>
+    );
+  }
+
   const useStyles = makeStyles({
     root: {
       width: '100%',
-      overflowX: 'auto',
     },
     table: {
-      minWidth: 650,
+      minWidth: 500,
+    },
+    tableWrapper: {
+      overflowX: 'auto',
     },
   });
 
   const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, results.length - page * rowsPerPage);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   return (
   <Paper className={classes.root}>
-    <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-        <TableRow>
-            <TableCell>Nom</TableCell>
-            <TableCell align="right">Lien GitHub</TableCell>
-            <TableCell align="right">Catégories</TableCell>
-            <TableCell align="right">Durée</TableCell>
-            <TableCell align="right">Participants</TableCell>
-            <TableCell align="right">Langue</TableCell>
-            <TableCell align="right">Kit à télécharger</TableCell>
-        </TableRow>
-        </TableHead>
-        <TableBody>
-        {results.map(row => (
-            <TableRow key={row.name}>
-            <TableCell component="th" scope="row">
-                {row.name}
-            </TableCell>
-            <TableCell align="right">{row.link}</TableCell>
-            <TableCell align="right">{row.tags}</TableCell>
-            <TableCell align="right">{row.duration}</TableCell>
-            <TableCell align="right">{row.people}</TableCell>
-            <TableCell align="right">{row.language}</TableCell>
-            <TableCell align="right"><a href={row.kit} title={row.kit}>Lien</a></TableCell>
-            </TableRow>
-        ))}
-        </TableBody>
-    </Table>
+      <div className={classes.tableWrapper}>
+          <Table className={classes.table} aria-label="simple table">
+              <TableHead>
+                  <TableRow>
+                      <TableCell>Nom</TableCell>
+                      <TableCell align="right">Lien GitHub</TableCell>
+                      <TableCell align="right">Catégories</TableCell>
+                      <TableCell align="right">Durée</TableCell>
+                      <TableCell align="right">Participants</TableCell>
+                      <TableCell align="right">Langue</TableCell>
+                      <TableCell align="right">Kit à télécharger</TableCell>
+                  </TableRow>
+              </TableHead>
+              <TableBody>
+              {(rowsPerPage > 0
+                ? results.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                : results
+              ).map(row => (
+                  <TableRow key={row.name}>
+                      <TableCell component="th" scope="row">
+                          {row.name}
+                      </TableCell>
+                      <TableCell align="right"><a href={row.link} title={row.link}>Homepage</a></TableCell>
+                      <TableCell align="right">{row.tags}</TableCell>
+                      <TableCell align="right">{row.duration}</TableCell>
+                      <TableCell align="right">{row.people}</TableCell>
+                      <TableCell align="right">{row.language}</TableCell>
+                      <TableCell align="right"><a href={row.kit} title={row.kit}>Lien</a></TableCell>
+                  </TableRow>
+              ))}
+
+              {emptyRows > 0 && (
+                  <TableRow style={{ height: 53 * emptyRows }}>
+                      <TableCell colSpan={6} />
+                  </TableRow>
+              )}
+              </TableBody>
+              <TableFooter>
+                  <TableRow>
+                      <TablePagination
+                        rowsPerPageOptions={[5, 10, 25, { label: 'Tout', value: -1 }]}
+                        colSpan={3}
+                        count={results.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        SelectProps={{
+                          inputProps: { 'aria-label': 'Résultats par page' },
+                          native: true,
+                        }}
+                        onChangePage={handleChangePage}
+                        onChangeRowsPerPage={handleChangeRowsPerPage}
+                        ActionsComponent={TablePaginationActions}
+                        labelDisplayedRows={({ from, to, count }) => from + '-' + to === -1 ? count : to + ' sur ' +count}
+                        labelRowsPerPage={'Résultats par page :'}
+                      />
+                  </TableRow>
+              </TableFooter>
+          </Table>
+      </div>
 </Paper>)
 }
 export default TableResults
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import TableResults from "../components/table-results"
 
 const IndexPage = () => {
       function createData(name, link, tags, duration, people, language, kit) {
-        return { name, link, tags, duration, people, language, kit };
+          return { name, link, tags, duration, people, language, kit };
       }
       
       const rows = [
@@ -14,12 +14,20 @@ const IndexPage = () => {
         createData('Retrospective Starter Kit', 'https://kenji-getpowered.github.io/retrospective-starter-kit/', 'Retro', 'Tous', 'Tous', 'fr', 'http://www.filedropper.com/scrum-retrospective-starter-kit'),
         createData('Code-moi un mouton', 'https://github.com/zenika-open-source/code-moi-un-mouton', 'School', 'Tous', 'Tous', 'fr', 'https://github.com/zenika-open-source/code-moi-un-mouton/tree/master/kit-de-demarrage'),
         createData('Elephant Carpaccio', 'https://github.com/jlandure/techio-carpaccio-programming/blob/master/intro.md', 'Kata', 'Tous', 'Tous', 'fr', 'https://github.com/jlandure/techio-carpaccio-programming/blob/master/product.md'),
+        createData('Tennis Refactoring Kata', 'https://github.com/emilybache/Tennis-Refactoring-Kata', 'Kata', 'Tous', 'Tous', 'en', 'https://github.com/emilybache/Tennis-Refactoring-Kata/blob/master/README.md'),
+        createData('Racing Car Katas', 'https://github.com/emilybache/Racing-Car-Katas', 'Kata', 'Tous', 'Tous', 'en', 'https://github.com/emilybache/Racing-Car-Katas/blob/master/README.md'),
+        createData('Parrot Refactoring Kata', 'https://github.com/emilybache/Parrot-Refactoring-Kata', 'Kata', 'Tous', 'Tous', 'en', 'https://github.com/emilybache/Parrot-Refactoring-Kata/blob/master/README.md'),
+        createData('Supermarket Receipt Refactoring Kata', 'https://github.com/emilybache/SupermarketReceipt-Refactoring-Kata', 'Kata', 'Tous', 'Tous', 'en', 'https://github.com/emilybache/SupermarketReceipt-Refactoring-Kata/blob/master/README.md'),
+        createData('Supermarket Pricing', 'https://github.com/raddanesh/Kata01', 'Kata', 'Tous', 'Tous', 'en', 'https://github.com/raddanesh/Kata01/blob/master/README.md'),
+        createData('ALphabet Cipher', 'https://github.com/gigasquid/wonderland-clojure-katas/tree/master/alphabet-cipher', 'Kata', 'Tous', 'Tous', 'en', 'https://github.com/gigasquid/wonderland-clojure-katas/blob/master/alphabet-cipher/README.md'),
         createData('Gilded Rose Kata', 'https://github.com/emilybache/GildedRose-Refactoring-Kata', 'Kata', 'Tous', 'Tous', 'en,fr', 'https://github.com/emilybache/GildedRose-Refactoring-Kata/blob/master/GildedRoseRequirements_fr.md'),
         createData('Theatrical Players Refactoring Kata', 'https://github.com/emilybache/Theatrical-Players-Refactoring-Kata', 'Kata', 'Tous', 'Tous', 'en', 'https://github.com/emilybache/Theatrical-Players-Refactoring-Kata/blob/master/README.md'),
         createData('Agile Game of Thrones', 'https://github.com/open-agile/agile-game-of-thrones', 'Priorisation', '1h30', '2-30', 'fr', 'https://github.com/open-agile/agile-game-of-thrones/tree/master/long_version_got'),
-        createData('Lego Carpaccio', 'https://github.com/open-agile/lego-carpaccio', 'Iteration,Feedback,Lego', '1h30 mn', '2-30', 'fr', 'https://github.com/open-agile/lego-carpaccio'),
+        createData('Lego Carpaccio', 'https://github.com/open-agile/lego-carpaccio', 'Iteration,Feedback,Lego', '1h30', '2-30', 'fr', 'https://github.com/open-agile/lego-carpaccio'),
         createData('RetroThemez', 'https://github.com/cs91/RetroThemez', 'Retro', 'Tous', 'Tous', 'en', 'https://github.com/cs91/RetroThemez'),
         createData('Rétrospectives Scénarisées', 'https://github.com/chapuyj/scripted-retrospective', 'Retro', 'Tous', 'Tous', 'fr', 'https://github.com/chapuyj/scripted-retrospective/blob/master/scripts/kaamelott_un-sejour-a-kaamelott_20191026.md'),
+        createData('Open Practice Library', 'https://openpracticelibrary.com/', 'Retro', 'Tous', 'Tous', 'en', 'https://openpracticelibrary.com/practice/example-mapping/'),
+        //createData('', '', '', '', '', '', ''),
       ];
 
     return (<Layout>
